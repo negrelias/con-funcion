89df8799fae90629f5df86bf5e8ba6dd3c01721d
Elias Wagenmacker <85260898+negrelias@users.noreply.github.com>
2021-12-06 11:32:06 +0000

    fullstack

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carousel-pause").click(function(){
+        $("#mycarousel").carousel('pause');
+    });
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+});
+
+
+
+//modal login//
+
+$(document).ready(function(){
+$("#myBtn").click(function(){
+$("#myModal").modal();
+});
+});
+
+
+
+// modal reserve//
+
+
+$(document).ready(function(){
+$("#mybtn3").click(function(){
+$("#ModalCenter").modal();
+});
+});
+
